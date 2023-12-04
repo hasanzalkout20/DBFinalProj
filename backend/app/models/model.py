e12281852d6e12ed8a40b6552f360e030e2c0fe6
Hasan Zalkout <hasan.zalkout@gmail.com>
2023-12-04 11:46:32 -0600

    data entry methods

--- backend/app/models/model.py
+++ backend/app/models/model.py
@@ -78,4 +78,59 @@ class Model:
         programs = self.cursor.fetchall()
         return programs
 
+# DATA ENTRY METHODS
+
+    # Method to insert a new department
+    def insert_department(self, deptName, deptCode):
+        sql = "INSERT INTO Department (DeptName, DeptCode) VALUES (%s, %s)"
+        self.cursor.execute(sql, (deptName, deptCode))
+        self.connection.commit()
+
+    # Method to insert a new faculty member
+    def insert_faculty(self, name, email, deptID, position):
+        sql = "INSERT INTO Faculty (Name, Email, DeptID, Position) VALUES (%s, %s, %s, %s)"
+        self.cursor.execute(sql, (name, email, deptID, position))
+        self.connection.commit()
+
+    # Method to insert a new program
+    def insert_program(self, progName, deptID, facultyLeadID, facultyLeadEmail):
+        sql = "INSERT INTO Program (ProgName, DeptID, FacultyLeadID, FacultyLeadEmail) VALUES (%s, %s, %s, %s)"
+        self.cursor.execute(sql, (progName, deptID, facultyLeadID, facultyLeadEmail))
+        self.connection.commit()
+
+    # Method to insert a new course
+    def insert_course(self, deptID, title, description):
+        sql = "INSERT INTO Course (DeptID, Title, Description) VALUES (%s, %s, %s)"
+        self.cursor.execute(sql, (deptID, title, description))
+        self.connection.commit()
+
+    # Method to insert a new section
+    def insert_section(self, courseID, semester, year, facultyLeadID, enrollCount):
+        sql = "INSERT INTO Section (CourseID, Semester, Year, FacultyLeadID, EnrollCount) VALUES (%s, %s, %s, %s, %s)"
+        self.cursor.execute(sql, (courseID, semester, year, facultyLeadID, enrollCount))
+        self.connection.commit()
+
+    # Method to insert a new objective
+    def insert_objective(self, objCode, description, deptID):
+        sql = "INSERT INTO Objectives (ObjCode, Description, DeptID) VALUES (%s, %s, %s)"
+        self.cursor.execute(sql, (objCode, description, deptID))
+        self.connection.commit()
+
+    # Method to insert a new subobjective
+    def insert_subobjective(self, subObjCode, description, parentObjID):
+        sql = "INSERT INTO SubObjectives (SubObjCode, Description, ParentObjID) VALUES (%s, %s, %s)"
+        self.cursor.execute(sql, (subObjCode, description, parentObjID))
+        self.connection.commit()
+
+    # Method to link a course to an objective
+    def link_course_to_objective(self, courseID, objID):
+        sql = "INSERT INTO CourseObjectives (CourseID, ObjID) VALUES (%s, %s)"
+        self.cursor.execute(sql, (courseID, objID))
+        self.connection.commit()
+
+    # Method to enter evaluation results for a section
+    def insert_evaluation_result(self, courseObjID, secID, semester, year, evalMethod, studentsPassed):
+        sql = "INSERT INTO ObjectiveEval (CourseObjID, SecID, Semester, Year, EvalMethod, StudentsPassed) VALUES (%s, %s, %s, %s, %s, %s)"
+        self.cursor.execute(sql, (courseObjID, secID, semester, year, evalMethod, studentsPassed))
+        self.connection.commit()
     
